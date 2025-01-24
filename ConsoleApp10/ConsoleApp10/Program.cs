1b75e4bb60c5c3cc83ee735ae1ae7a62b1e1c46e
Sashakrm <sasakramch@gmail.com>
2025-01-24 10:39:56 +0200

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cicle();
+            Console.Read();
+        }
+
+        static void Cicle()
+        {
+            for (int x = 1; x <= 4; x++)
+            {
+                for (int a = 1; a <= 10; a++)
+                {
+                    Console.Write(a);
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}

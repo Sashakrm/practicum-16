0699d52433e8295f969dd8b3b77cbfbaa5a5f05e
Sashakrm <sasakramch@gmail.com>
2025-01-25 22:49:07 +0200

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cicle();
+            Console.Read();
+        }
+
+        static void Cicle()
+        {
+            int x,a,y,value;
+            for (x = 4; x <= 7; x++)
+            {
+                for (a = 1; a <= 10; a++)
+                {
+                    y = x * 10;
+                    value = y + a;
+                    Console.Write($"{value} ");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}

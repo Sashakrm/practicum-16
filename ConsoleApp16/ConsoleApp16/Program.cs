565445957684fd05388e625c25df979ecb80cf4a
Sashakrm <sasakramch@gmail.com>
2025-01-25 23:51:13 +0200

    Add files via upload

---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Cicle();
+        Console.Read();
+    }
+
+    static void Cicle()
+    {
+        int x = 8;
+        for (int y = 0; y <= x; y++)
+        {
+            Console.Write(new string(' ', y));
+            Console.WriteLine("#");
+        }
+    }
+}
